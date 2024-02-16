19d01919f05a95ef9ed93e4feaf6b250728cc833
decoy1337 <leilichjaysin@rambler.ru>
2024-02-16 18:03:11 +0300

    fhdh

--- src/game-models/Hero.js
+++ src/game-models/Hero.js
@@ -1,9 +1,10 @@
 // Наш герой.
 
 class Hero {
-  constructor({ position }) {
+  constructor(boomerang) {
     this.skin = '🤠'; // можете использовать любые emoji '💃'
-    this.position = position;
+    this.position = 10;
+    this.boomerang=boomerang
   }
 
   moveLeft() {
@@ -19,6 +20,7 @@ class Hero {
   attack() {
     // Атакуем.
     this.boomerang.fly();
+    
   }
 
   die() {
